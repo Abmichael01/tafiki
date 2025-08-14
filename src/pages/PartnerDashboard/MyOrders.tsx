98d861d8962685f3ee066fc07eab271541fd1730
Urkel <abmichael109@gmail.com>
2025-08-14 15:30:56 +0100

    new changes

--- src/pages/PartnerDashboard/MyOrders.tsx
+++ src/pages/PartnerDashboard/MyOrders.tsx
@@ -14,7 +14,7 @@ const MyOrders: React.FC = () => {
   const currentTab = params.get("tab");
   const filteredOrders = orders?.filter( order => {
     if (currentTab === "processing") return order.status === "pending";
-    if (currentTab === "ongoing") return order.status !== "pending" || order.status === "settled";
+    if (currentTab === "ongoing") return order.status !== "pending" || "settled";
     if (currentTab === "history") return order.status === "settled";
   })
 
