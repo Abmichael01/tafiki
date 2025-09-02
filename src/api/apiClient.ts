7d48b33839f7daf0227fa47837b5e12c419b38b8
Urkelcodes <abmichael109@gmail.com>
2025-09-02 03:22:20 +0100

    any

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import useAuthStore from "@/stores/authStore";
 
 // export const API_BASE_URL = import.meta.env.VITE_API_URL 
-export const API_BASE_URL = "http://127.0.0.1:8000/api"
+export const API_BASE_URL = "https://food-hybrid-backend.vercel.app/api"
 
 // Create Axios instance
 const apiClient = axios.create({
