ca63f67f51af87d141a78aa2655e9ced5b6c5bde
Urkelcodes <abmichael109@gmail.com>
2025-09-14 16:42:56 +0100

    removed .env

--- src/components/Site/Home/Hero.tsx
+++ src/components/Site/Home/Hero.tsx
@@ -1,25 +1,32 @@
 import { IoGlobeOutline } from "react-icons/io5";
 import { motion } from "framer-motion";
-import { fadeInUp, slideInLeft, slideInRight, staggerContainer, zoomIn } from "@/lib/animations";
+import {
+  fadeInUp,
+  slideInLeft,
+  slideInRight,
+  staggerContainer,
+  zoomIn,
+} from "@/lib/animations";
+import { Link } from "react-router-dom";
 
 const statistics = [
   { number: "100+", label: "Orders Delivered" },
   { number: "3000+", label: "Virtual Distributors" },
   { number: "2000+", label: "Retail Shops" },
-  { number: "5+", label: "Vendors" }
-]
+  { number: "5+", label: "Vendors" },
+];
 
 export default function Hero() {
   return (
     <div className="relative flex items-center justify-between w-full gap-[30px] min-h-[700px]">
-      <motion.div 
+      <motion.div
         initial="initial"
         whileInView="animate"
         viewport={{ once: true, amount: 0.3 }}
         variants={slideInLeft}
         className=" section-padding-left max-w-[90%] sm:max-w-[80%] md:max-w-[60%] lg:max-w-[50%] relative z-[1] mt-40"
       >
-        <motion.h1 
+        <motion.h1
           initial="initial"
           whileInView="animate"
           viewport={{ once: true, amount: 0.3 }}
@@ -28,7 +35,7 @@ export default function Hero() {
         >
           Smarter Food Logistics
         </motion.h1>
-        <motion.p 
+        <motion.p
           initial="initial"
           whileInView="animate"
           viewport={{ once: true, amount: 0.3 }}
@@ -39,40 +46,44 @@ export default function Hero() {
           local communities, and bringing Africa's rich food legacy to the
           world.
         </motion.p>
-        <motion.button 
-          initial="initial"
-          whileInView="animate"
-          viewport={{ once: true, amount: 0.3 }}
-          variants={zoomIn}
-          className="flex items-center gap-2 px-4 py-2 bg-primary rounded-full text-white mt-[40px]"
-        >
-          <span>Get Started</span>
-          <IoGlobeOutline />
-        </motion.button>
-        
-        <motion.div 
+
+        <Link to="/partner/register?stage=1">
+          <motion.button
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true, amount: 0.3 }}
+            variants={zoomIn}
+            className="flex items-center gap-2 px-4 py-2 bg-primary rounded-full text-white mt-[40px]"
+          >
+            <span>Get Started</span>
+            <IoGlobeOutline />
+          </motion.button>
+        </Link>
+
+        <motion.div
           initial="initial"
           whileInView="animate"
           viewport={{ once: true, amount: 0.3 }}
           variants={staggerContainer}
           className="mt-16 font-satoshi"
         >
-          <motion.p 
-            variants={fadeInUp}
-            className="text-[#5D5D5D] text-sm mb-4"
-          >
+          <motion.p variants={fadeInUp} className="text-[#5D5D5D] text-sm mb-4">
             With over:
           </motion.p>
           <div className="grid grid-cols-4 gap-4">
             {statistics.map((stat, index) => (
-              <motion.div 
-                key={index} 
+              <motion.div
+                key={index}
                 variants={fadeInUp}
                 custom={index}
                 className="text-start"
               >
-                <h3 className="text-[19px] sm:text-[24px] font-satoshi-bold font-[700] text-[#86BF4C] ">{stat.number}</h3>
-                <p className="text-[#5D5D5D] text-xs font-[500]">{stat.label}</p>
+                <h3 className="text-[19px] sm:text-[24px] font-satoshi-bold font-[700] text-[#86BF4C] ">
+                  {stat.number}
+                </h3>
+                <p className="text-[#5D5D5D] text-xs font-[500]">
+                  {stat.label}
+                </p>
               </motion.div>
             ))}
           </div>
