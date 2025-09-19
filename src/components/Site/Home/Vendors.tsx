0e3574b2ee6c83cbe31f84c34aabe36385e2c4fd
Urkelcodes <abmichael109@gmail.com>
2025-09-19 13:55:06 +0100

    hello there

--- src/components/Site/Home/Vendors.tsx
+++ src/components/Site/Home/Vendors.tsx
@@ -1,26 +1,58 @@
-import { motion } from 'framer-motion'
+// import { motion } from 'framer-motion'
 
 const vendors = [
-  { name: "FoodHybrid", logo: "/foodhybrid.svg" },
-  { name: "AgriCorp", logo: "/foodhybrid.svg" },
-  { name: "FarmFresh", logo: "/foodhybrid.svg" },
-  { name: "GreenHarvest", logo: "/foodhybrid.svg" },
-  { name: "OrganicPlus", logo: "/foodhybrid.svg" },
-  { name: "NatureFoods", logo: "/foodhybrid.svg" },
-  { name: "PureHarvest", logo: "/foodhybrid.svg" },
-  { name: "EcoFarms", logo: "/foodhybrid.svg" }
-]
+  { name: "FoodHybrid", logo: "/foodhybrid.png" },
+  { name: "FoodCore", logo: "/foodcore.jpg" },
+];
+
+// Full vendors array for scrolling (commented out for now)
+// const allVendors = [
+//   { name: "FoodHybrid", logo: "/foodhybrid.svg" },
+//   { name: "AgriCorp", logo: "/foodhybrid.svg" },
+//   { name: "FarmFresh", logo: "/foodhybrid.svg" },
+//   { name: "GreenHarvest", logo: "/foodhybrid.svg" },
+//   { name: "OrganicPlus", logo: "/foodhybrid.svg" },
+//   { name: "NatureFoods", logo: "/foodhybrid.svg" },
+//   { name: "PureHarvest", logo: "/foodhybrid.svg" },
+//   { name: "EcoFarms", logo: "/foodhybrid.svg" }
+// ]
 
 export default function Vendors() {
   return (
     <div className="section-padding mt-30 flex flex-col gap-5 mask-l-from-80% mask-l-to-90% mask-r-to-90% mask-r-from-80%">
-        <h1 className="font-[900] font-satoshi text-[#5D5D5D] text-[20px] text-center">Our Trusted Vendors</h1>
-        
+      <h1 className="font-[900] font-satoshi text-[#5D5D5D] text-[20px] text-center">
+        Our Trusted Vendors
+      </h1>
+
+      {/* Static display of two vendor logos */}
+      <div className="flex items-center justify-center gap-8">
+        {/* {vendors.map((vendor, index) => (
+            <img 
+              key={index}
+              src={vendor.logo} 
+              alt={vendor.name} 
+              className="h-[60px] flex-shrink-0" 
+            />
+          ))} */}
+        <img
+          src={"/foodhybrid.png"}
+          alt="FoodHybrid"
+          className="h-[60px] flex-shrink-0"
+        />
+        <img
+          src={"/foodcore.jpg"}
+          alt="FoodCore"
+          className="h-[100px] flex-shrink-0"
+        />
+      </div>
+
+      {/* Scrolling animation code (commented out for now) */}
+      {/* 
         <div className="overflow-hidden">
           <motion.div 
             className="flex gap-8"
             animate={{ 
-              x: [0, -100 * vendors.length]
+              x: [0, -100 * allVendors.length]
             }}
             transition={{ 
               x: {
@@ -32,7 +64,7 @@ export default function Vendors() {
             }}
           >
             {/* First set of logos */}
-            {vendors.map((vendor, index) => (
+      {/* {allVendors.map((vendor, index) => (
               <img 
                 key={`first-${index}`}
                 src={vendor.logo} 
@@ -41,7 +73,7 @@ export default function Vendors() {
               />
             ))}
             {/* Duplicate set for seamless loop */}
-            {vendors.map((vendor, index) => (
+      {/* {allVendors.map((vendor, index) => (
               <img 
                 key={`second-${index}`}
                 src={vendor.logo} 
@@ -51,6 +83,7 @@ export default function Vendors() {
             ))}
           </motion.div>
         </div>
+        */}
     </div>
-  )
+  );
 }
